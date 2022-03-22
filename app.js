ddf5c858121a156246cce868712c3e23a981b2db
pengpeng <s123171293@gmail.com>
2022-03-22 23:47:20 +0800

    littke change

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ app.get('/', (req, res) => {
     res.send("You are in the first page!");
 });
 
+
+app.get('/daniel', (req, res) => {
+    res.send("this is daine pages");
+});
+
+app.get('/jack', (req, res) => {
+    res.send("this is jack daine pages!");
+});
+
+
 app.listen(3000, () => {
     console.log("serve is listening on port 3000");
 });
